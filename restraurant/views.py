92e6a7e3af09f7307c10b2e17ec9f8722ece67bd
Xuebin Ran <ranxuebin@ranxb.cn>
2019-01-09 00:52:14 +0800

    基础查询功能，现在开始做前端，前段逻辑做好之后，在做业务逻辑。

--- restraurant/views.py
+++ restraurant/views.py
@@ -1,3 +1,23 @@
+import json
+
+from django.http import HttpResponse
+from restraurant.models import Restaurant
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    response1 = ""
+
+    # list = Restaurant.objects.order_by('name')[0:2]
+    data = Restaurant.objects.filter(name__contains="Roc")
+    # for p in list:
+    #     response1 += p.name +" "+p.price_range+ "<br>"
+    # response = response1
+
+    for i in data:
+        response1 +=i.name
+
+    content = {"data" : response1}
+
+    print(response1)
+    return render(request, "index.html",content)
